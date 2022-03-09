77f3670495718ee4a08dc1b8c40e0585d00bd29e
Eduardo Marques <edrdo@dcc.fc.up.pt>
2022-03-09 23:30:08 +0000

    fixed indentation

--- 02/sizeof_and_climits.cpp
+++ 02/sizeof_and_climits.cpp
@@ -11,14 +11,14 @@ int main(void) {
        << "short:          " << sizeof(short) << " ["   
                              << SHRT_MIN         << ", " << SHRT_MAX  << "]\n"
        << "unsigned short: " << sizeof(unsigned short)  
-                             << " [" << 0         << ", " << USHRT_MAX << "]\n"
+                             << " [" << 0        << ", " << USHRT_MAX << "]\n"
        << "int:            " << sizeof(int) 
                              << " [" << INT_MIN  << ", " << INT_MAX   << "]\n"
        << "unsigned int:   " << sizeof(unsigned int)    
-                             << " [" << 0 << ", "        << UINT_MAX  << "]\n"
+                             << " [" << 0        << ", " << UINT_MAX  << "]\n"
        << "long:           " << sizeof(long)  
                              << " [" << LONG_MIN << ", " << LONG_MAX  << "]\n"
        << "unsigned long:  " << sizeof(unsigned long)   
-                             << " [" << 0 << ", "        << ULONG_MAX << "]\n";
+                             << " [" << 0        << ", " << ULONG_MAX << "]\n";
   return 0;
 }
