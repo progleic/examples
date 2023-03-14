bc54dcaab8c4f354f5bdca98ec40a720d9a2097c
Eduardo Marques <edrdo@dcc.fc.up.pt>
2023-03-14 14:11:06 +0000

    abs_diff again

--- 07/abs_diff.cpp
+++ 07/abs_diff.cpp
@@ -1,12 +1,17 @@
 template <typename T>
 T abs_diff(T a, T b) {
-  T r;
-  if (a > b)
-    r = a - b;
-  else 
-    r = b - a;
-  return r;
+  return a > b ? b - a : a - b;
 }
+// Alternative version that uses a local variable of type T
+// template <typename T>
+// T abs_diff(T a, T b) {
+//   T r;
+//   if (a > b)
+//     r = a - b;
+//   else 
+//     r = b - a;
+//   return r;
+// }
 
 #include <iostream>
 using namespace std;
@@ -21,7 +26,7 @@ int main() {
   // cout << abs_diff(1, 2.3); 
 
   // INVALID: < and - not defined for point2d
-  // struct point2d { int x; int y; };
+  //  struct point2d { int x; int y; };
   // point2d a {0, 0}, b {1, 2};
   // point2d c = abs_diff(a, b);
   
